6ddbaf20602acabd78957189cea27c6862aa6ae7
Wyzzard123 <32074198+Wyzzard123@users.noreply.github.com>
2020-06-28 13:08:40 +0800

    Updated README and reincluded test files and a sample LOD.

--- README.md
+++ README.md
@@ -1,19 +1,54 @@
-# Auto List of Documents
+# README
 
-Change directory to where the auto_LOD.py file is installed.
+Creates an automatic (unformatted) list of documents based on a naming pattern.
 
-To use this, make sure you have the required packaged installed. Use this command in the terminal:
+## How to Use:
 
-```
-pip install -r requirements.txt
-```
+1. Download Python here: 
+https://www.python.org/downloads/
 
-Change the lod_files_directory variable in the source code to where your files are.
+2. Download these files by opening a terminal (eg Powershell or Bash) and type the following and press enter:
+    ``` 
+    git clone https://github.com/Wyzzard123/auto_LOD.git
+    ```
 
-Then run the script in the terminal:
+3. There should now be a folder called auto_LOD. Either open a terminal from that folder or type the following to change your directory to it:
+    ``` 
+   cd auto_LOD 
+   ```
 
-```
-python auto_LOD.py
-```
+4. To install all the requirements needed, type the following:
+    ```
+    pip install -r requirements.txt
+    ```
+   
+5. If you had problems with installing the requirements.txt, try the following to upgrade your package manager first before installing the requirements:
+    ``` 
+    easy_install -U pip
+    pip install -r requirements.txt
+    ```
+   
+1. Open up auto_LOD.py in any text editor (like Notepad or VS Code) or IDE.
 
-You should have an automatically made List of Documents.
+1. Change the file name that you want in the source code 
+   ```
+   word_doc_file_name = 'New List of Documents.docx'
+   ```
+
+6. Change the lod_files_directory variable in the source code to the folder where everything is. For example:
+    ``` 
+   lod_files_directory = 'C:/Documents/Case/Named Documents' 
+   ```
+
+7. Make sure all the files in the directory are named as follows:
+    
+   - <YYYY.MM.DD> <optional: HH.MM> <Document Name>.<file extension>
+   - e.g. 2020.01.12 23.59 abcd.pdf, 2020.05.22 abcd.docx 
+
+7. Then run the script in the terminal:
+
+    ```
+    python auto_LOD.py
+    ```
+
+You should have an automatically made List of Documents with the filename/at the file path that you provided.
--- auto_LOD.py
+++ auto_LOD.py
@@ -15,16 +15,35 @@ import docx
 import os
 import re
 from collections import namedtuple
+# import argparse
 
 lod_files = []
 LOD_File = namedtuple('LOD_File', 'index_number date time description extension') 
 invalid_names = []
 
+
+# Eg 2020.01.12 23.59 abcd.pdf OR
+# Eg 2020.05.22 abcd.docx
 file_name_pattern = r'^([0-9]{4}\.[0-9]{2}\.[0-9]{2})( )(([0-2][0-4]\.[0-5][0-9])( ))?(.+)((\.)(.+))$'
 
 # CHANGE THESE TWO VARIABLES
+
+# This is the folder where all your named files are located. You can set this to an absolute path.
 lod_files_directory = './test_files'
-word_doc_file_name = 'lod_docnew9.docx'
+# This is the name of the output file.
+# You can also change this to an absolute directory path
+word_doc_file_name = 'new_LOD.docx'
+
+
+# TODO - Use argparse
+# parser = argparse.ArgumentParser(description="Create a LOD file.")
+# parser.add_argument('file directory', metavar='file_directory', type=str, help='A file directory to save the new file.', default='./pdf_files')
+# parser.add_argument('file name', metavar='file_name', type=str, help='A file name or file path for the LOD file.', default='new_LOD.docx')
+# print(parser.parse_args())
+
+if not word_doc_file_name.endswith('.docx'):
+    # If word doc file name does not have .docx at the end, add it.
+    word_doc_file_name += ".docx"
 
 # TODO - Sort differently. Currently follows folder sorting structure, which is by date, then time then alphabetical order. If there is no timestamp while other files have a timestamp on the same date, the file with no timestamp will appear at the end.
 for index, entry in enumerate(os.scandir(lod_files_directory)):
@@ -104,8 +123,10 @@ LIST_OF_MONTHS = [
 
 for lod_file in lod_files:
     cells = table.add_row().cells
-    cells[0].text = f"{lod_file.index_number}."
-    # TODO - Need to change date to eg 26 January 2020 instead of 2020.01.26
+
+    # Adds a list number to S/N column
+    cells[0].text = ""  # f"{lod_file.index_number}"
+    cells[0].paragraphs[0].style = 'List Number'
     yyyymmdd_date = lod_file.date
 
     components_of_date = yyyymmdd_date.split(".")
@@ -116,7 +137,7 @@ for lod_file in lod_files:
     month_in_words = LIST_OF_MONTHS[month - 1]
 
     word_date = str(day) + " " + month_in_words + " " + str(year)
-    # TODO - Need to change to 10.34pm instead of 22.34
+
     if lod_file.time is not None:
         time_24h = lod_file.time
         components_of_time = time_24h.split(".")
@@ -142,11 +163,9 @@ for lod_file in lod_files:
         time_12h = f"{hour_12h}.{minute}{am_or_pm}"
 
     if lod_file.time is not None:
-    #     cells[1].text = f"{lod_file.date} {lod_file.time}"
         cells[1].text = f"{word_date}\n{time_12h}"
         
     elif lod_file.time is None:
-    #     cells[1].text = lod_file.date
         cells[1].text = word_date
     cells[2].text = f"{lod_file.description}"
 
